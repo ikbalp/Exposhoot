59b3618f368b60338764c36602da5be5276f0c7d
ikbalp <63147281+ikbalp@users.noreply.github.com>
2020-07-03 15:31:51 +0700

    fragment menu

---
+++ app/src/main/java/com/ikbalp/exposhoot/FragTips.java
@@ -0,0 +1,34 @@
+package com.ikbalp.exposhoot;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+
+/*
+     Minggu, 10 Mei 2020
+     Ikbal Padilah
+     10117143 - IF4
+*/
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragTips extends Fragment {
+
+    public FragTips() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tips, container, false);
+    }
+}
--- app/src/main/java/com/ikbalp/exposhoot/MainActivity.java
+++ app/src/main/java/com/ikbalp/exposhoot/MainActivity.java
@@ -1,8 +1,13 @@
 package com.ikbalp.exposhoot;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 /*
      Kamis, 25 Juni 2020
@@ -13,9 +18,56 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
+    BottomNavigationView bottomNavigationView;
+
+    FragHome fragHome;
+    FragCamera fragCamera;
+    FragTips fragTips;
+    FragSearch fragSearch;
+
+    MenuItem menuItem;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView navigationView = findViewById(R.id.bottom_nav);
+        final FragHome fragHome = new FragHome();
+        final FragCamera fragCamera = new FragCamera();
+        final FragTips fragTips = new FragTips();
+        final FragSearch fragSearch = new FragSearch();
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                if (id == R.id.home) {
+                    setFragment(fragHome);
+                    return  true;
+                } else if (id == R.id.camera) {
+                    setFragment(fragCamera);
+                    return  true;
+                } else if (id == R.id.tips) {
+                    setFragment(fragTips);
+                    return  true;
+                } else if (id == R.id.search) {
+                    setFragment(fragSearch);
+                    return  true;
+                }
+                return false;
+            }
+        });
+
+
+
+
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.frame, fragment);
+        fragmentTransaction.commit();
     }
 }
